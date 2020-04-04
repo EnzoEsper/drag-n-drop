fa002d523318e2bb425bf4e6373b1eb4272e6e4c
Enzo Esperguin <enzoesper@hotmail.com>
2020-04-04 09:27:43 -0300

    rendering the form in the browser when an object of the class ProjectInput is instantiated

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
 // Code goes here!
+class ProjectInput {
+  // elemento que se va a renderear dentro del elemento host
+  templateElement: HTMLTemplateElement;
+  element: HTMLFormElement;
+  hostElement: HTMLDivElement;
+
+  constructor() {
+    // cuando se obtienen los elementos del dom (por su id) no van a ser null y se los castea al tipo correspondiente
+    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    // crea una copia del nodo para luego ser insertado dentro del host (div)
+    const importedNode = document.importNode(this.templateElement.content, true);
+    // como el primer elemento siguiente del template es un form, se lo obtiene y se castea
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const projInput = new ProjectInput();
